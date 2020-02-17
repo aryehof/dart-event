43177e665ab6194d700d7748d0c8d8ad17bcf424
Aryeh <aryeh@users.noreply.github.com>
2020-02-17 09:35:05 +0200

    Changed event name per Event Naming Pattern

--- example/event_example.dart
+++ example/event_example.dart
@@ -3,21 +3,21 @@ import 'package:event/event.dart';
 /*
 Summary
 
-1. An Event onValueChanged is declared in the Counter class
+1. An Event 'valueChangedEvent' is declared in the Counter class
 2. It provides a custom argument to subscribers of the event
       as specified in the ValueEventArgs class at the
        bottom of this page.
    Note that providing an argument to an Event is optional.
 3. A subscriber to the Event is added in the main() method.
 4. The Event (with custom argument) is broadcast (notified) to
-      subscribers in the Counter's  increment and reset methods.
+      subscribers in the Counter's increment and reset methods.
 */
 
 void main() {
   var c = Counter();
 
   // Subscribe to the custom Event.
-  c.onValueChanged.subscribe((args) => print('value changed to ${args.changedValue}'));
+  c.valueChangedEvent.subscribe((args) => print('value changed to ${args.changedValue}'));
 
   // The '+' operator is a shortcut for the subscribe method.
   // It is directly equivalent to ...
@@ -42,20 +42,20 @@ class Counter {
   int value = 0;
 
   /// A custom [Event] of type [ValueEventArgs]
-  final onValueChanged = Event<ValueEventArgs>();
+  final valueChangedEvent = Event<ValueEventArgs>();
 
   /// Increment the [Counter] [value] by 1.
   void increment() {
     value++;
     // notify subscribers of the change in value
-    onValueChanged.broadcast(ValueEventArgs(value));
+    valueChangedEvent.broadcast(ValueEventArgs(value));
   }
 
   /// Reset the [Counter] [value] to 0.
   void reset() {
     value = 0;
     // notify subscribers of the change in value
-    onValueChanged.broadcast(ValueEventArgs(value));
+    valueChangedEvent.broadcast(ValueEventArgs(value));
   }
 }
 
