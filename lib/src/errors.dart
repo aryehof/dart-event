6460454cb1a0c90e02a2d1f7598198feae2142a4
Aryeh Hoffman <9958287+aryehof@users.noreply.github.com>
2024-09-10 14:27:01 +0300

    Add copyright header

--- lib/src/errors.dart
+++ lib/src/errors.dart
@@ -1,3 +1,7 @@
+// Copyright 2020 Aryeh Hoffman. All rights reserved.
+// Use of this source code is governed by an Apache-2.0 license that can be
+// found in the LICENSE file.
+
 /// An error that indicates that the argument being [broadcast]
 /// does not match the [Event] generic type.
 class ArgsError extends TypeError {
--- test/test_helpers.dart
+++ test/test_helpers.dart
@@ -1,3 +1,7 @@
+// Copyright 2020 Aryeh Hoffman. All rights reserved.
+// Use of this source code is governed by an Apache-2.0 license that can be
+// found in the LICENSE file.
+
 import 'dart:convert';
 import 'dart:io';
 
